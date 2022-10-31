ca7f5c31d44b3298d6c4933bbbb3ae18a7008bfb
arthurjolo <arthurjolo@gmail.com>
2022-10-31 15:25:55 -0300

    controle de concorrencia

--- lab2 - Atividade RMI/ActivityList.java
+++ lab2 - Atividade RMI/ActivityList.java
@@ -5,26 +5,42 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.ArrayList;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ActivityList extends UnicastRemoteObject implements ListInteface{
 	HashMap<Integer, String> lista;
+
+	private Lock lock = new ReentrantLock();
 	
 	public ActivityList() throws RemoteException {
+		
 		lista = new HashMap<Integer, String>();
+		Lock lock = new ReentrantLock();
+		
 	}
 
 	@Override
 	public void add(int index, Object element) throws RemoteException {
+		lock.lock();
 		lista.put(index, (String) element);
+		lock.unlock();
 	}
 
 	@Override
 	public Object get(int index) throws RemoteException {
-		return lista.get(index);
+		lock.lock();
+		Object element = lista.get(index);
+		lock.unlock();
+		return element;
 	}
 
 	@Override
 	public Object remove(int index) throws RemoteException {
-		return lista.remove(index);
+		lock.lock();
+		Object element = lista.remove(index);
+		lock.unlock();
+		return element;
 	}
 
 	@Override
